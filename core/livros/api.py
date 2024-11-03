35c21eb9123636098de0ca9cda4c0122b01ca496
blueIsaac1 <isaaccleitondasilva@gmail.com>
2024-11-03 00:38:39 -0300

    commit - fix

--- core/livros/api.py
+++ core/livros/api.py
@@ -4,14 +4,22 @@ from . models import Livros, Categories
 
 livros_router = Router()
 
+@livros_router.get('/', response={200: dict, 400: dict})
+def get_livro(request):
+    livros = Livros.objects.all()
+    # Serializando objetos usando LivrosSchema
+    livros_data = [LivrosSchema.from_orm(livro).dict() for livro in livros]
+    return {"response": livros_data}
+
+
 @livros_router.post('/', response={200: dict, 400: dict})
 def create_book(request, livro_schema: LivrosSchema):
     name = livro_schema.name
     streaming = livro_schema.streaming
     categories = livro_schema.categorie
 
-    if streaming not in ['FI', 'AK']:
-        return 400, {"status": "erro", "message": "Streaming inválido."}
+    if streaming not in ['FI', 'AK']:                   
+        return 400, {"status": "erro", "message": "Streaming inválido."}                        
 
     livro = Livros(name=name, streaming=streaming)
     livro.save()
@@ -20,7 +28,7 @@ def create_book(request, livro_schema: LivrosSchema):
         try:
             cat_temp = Categories.objects.get(id=cat_id)
             livro.categorie.add(cat_temp)
-        except Categories.DoesNotExist:
+        except Categories.DoesNotExist:         
             return 400, {"status": "erro", "message": f"Categoria com id {cat_id} não encontrada."}
 
     return 200, {"status": "ok", "message": "Livro criado com sucesso"}
@@ -45,10 +53,10 @@ def deletar_livros(request, livro_id: int):
 
     return 200, {'status': 'deleted', 'id': livro_id}
 
-@livros_router.get('/sortear/', response={200: LivrosSchema, 404:dict})
+@livros_router.get('/sortear/', response={200: LivrosSchema, 404: dict})
 def sortear_livro(request, filtros: Query[FiltrosSortear]):
     nota_minima = filtros.dict().get('nota_minima')
-    categorie = filtros.dict().get('categoria')
+    categoria = filtros.dict().get('categoria')
     reler = filtros.dict().get('reler', False)
 
     livros = Livros.objects.all()
@@ -58,14 +66,12 @@ def sortear_livro(request, filtros: Query[FiltrosSortear]):
     if nota_minima:
         livros = livros.filter(nota__gte=nota_minima)
     
-    if categorie:
-        livros = livros.filter(categorie__id=categorie)
+    if categoria:
+        livros = livros.filter(categorie__id=categoria)
 
     livro = livros.order_by('?').first()
 
-    if livros.count() > 0:
-        return 200, livro
+    if livro:
+        return 200, LivrosSchema.from_orm(livro).dict()
     else:
-        return 400, {'status', 'erro'}
-    
-    return{'ok', 'ok'}
+        return 404, {'status': 'erro', 'message': 'Nenhum livro encontrado.'}
